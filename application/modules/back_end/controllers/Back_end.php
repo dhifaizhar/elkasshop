59a0f84ad9a836426809117dafe14405d1231eb8
mustofaYR <rmustofayuli@gmail.com>
2016-02-12 10:48:31 +0700

    EDIT CONTROLLER

--- application/modules/back_end/controllers/Back_end.php
+++ application/modules/back_end/controllers/Back_end.php
@@ -1,11 +1,10 @@
-
 <?php 
 if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 class Back_End extends MX_Controller{
-
-	
     public function __construct(){
         parent::__construct();
+		
+		 $this->load->library('template');
     }
 
     public function index(){
